6014c4e6af4bcbd23cac10e48bdb4905480e1b5f
pettei47 <kitagawa.teppei.33c@gmail.com>
2021-08-28 13:12:01 +0900

    memcpyの不要な変数を削除

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/06 22:11:30 by tkitagaw          #+#    #+#             */
-/*   Updated: 2021/06/14 14:49:45 by teppei           ###   ########.fr       */
+/*   Updated: 2021/08/28 13:11:47 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	unsigned char	*p;
 	unsigned char	*q;
-	size_t			i;
 
 	p = (unsigned char *)dst;
 	q = (unsigned char *)src;
-	i = 0;
 	if (dst == src)
 		return (dst);
 	while (n > 0)
