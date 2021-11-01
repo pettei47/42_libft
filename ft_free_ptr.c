4bed88ea7fb7f9cde23c7d1cab881b1852d3c948
pettei47 <kitagawa.teppei.33c@gmail.com>
2021-11-02 01:48:19 +0900

    free_ptrでnull埋

--- ft_free_ptr.c
+++ ft_free_ptr.c
@@ -6,7 +6,7 @@
 /*   By: teppei <teppei@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:24:16 by teppei            #+#    #+#             */
-/*   Updated: 2021/10/17 23:04:06 by teppei           ###   ########.fr       */
+/*   Updated: 2021/11/02 01:43:44 by teppei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ void	ft_free_ptr(char **ptrs)
 		return ;
 	i = -1;
 	while (ptrs[++i])
+	{
 		free(ptrs[i]);
+		ptrs[i] = NULL;
+	}
 	free(ptrs);
 }
